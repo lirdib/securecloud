a4caec7e81ff1eb1ea4957715508814aa6b4547a
redhatalbania <38312870+lirdib@users.noreply.github.com>
2020-07-14 21:49:40 +0200

    testing templates

--- securecloud/urls.py
+++ securecloud/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', views.main)
+    path('main/', views.main),
+    path('', views.main),
 ]
--- securecloud/views.py
+++ securecloud/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+
+from django.shortcuts import render
 
 def main(request):
-    return HttpResponse('homepage')
+    return render(request, 'base_loyout.html')
