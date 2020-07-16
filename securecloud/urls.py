d72af04109bd05f64e63102b5569b7d6a2bc1f25
redhatalbania <38312870+lirdib@users.noreply.github.com>
2020-07-16 09:40:19 +0200

    authentication app register and main app deleted

--- securecloud/urls.py
+++ securecloud/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', views.main),
-    path('', views.main),
+    path('', include('authentication.urls')),
 ]
