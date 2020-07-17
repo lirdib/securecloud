0d6290d0d5f0af44fa3594b31b020146ed436643
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-17 11:47:03 +0200

    merge

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-]
-=======
 from django.urls import path,include
 from . import views
 
@@ -11,4 +5,3 @@ urlpatterns = [
 
     path('', views.signin),
 ]
->>>>>>> 9c55c653ed5cd63d594184549c1c3dccc21b43cc
