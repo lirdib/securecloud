3e12e1897f7a84b97b6748c84a1073e3f78f3fe4
redhatalbania <38312870+lirdib@users.noreply.github.com>
2020-07-18 15:44:49 +0200

    sign in form validated

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            return redirect('web/index.html')
+    else:
+        form = UserCreationForm()
     return  render(request,'authentication/signup.html', {'form':form})
