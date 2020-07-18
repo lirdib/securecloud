a8ed7819ab158c97e2de0ed595cef379f4512c4f
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2020-07-18 11:49:48 +0200

    create login

--- securecloud/urls.py
+++ securecloud/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('', include('web.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
