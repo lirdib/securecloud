e00d85b61f5f226b33c66c62d51202c2458fb170
redhatalbania <38312870+lirdib@users.noreply.github.com>
2020-07-20 21:07:21 +0200

    Form.save() added. Redirect url switched to 'index'

--- authentication/views.py
+++ authentication/views.py
@@ -6,9 +6,10 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
-            return redirect('web/index.html')
+            return redirect('index.html')
     else:
         form = UserCreationForm()
     return  render(request,'authentication/signup.html', {'form':form})
