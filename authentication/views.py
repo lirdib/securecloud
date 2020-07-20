e9f7c7c9ac864affbdc62615181b554cc7c87a5a
gouravkr <gourav2711@gmail.com>
2020-07-20 22:52:14 +0530

    added authentication requirement to chatroom

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -8,7 +8,7 @@ def signup(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}!')
-            return redirect('web/index.html')
+            return redirect('index')
     else:
         form = UserCreationForm()
     return  render(request,'authentication/signup.html', {'form':form})
--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required(login_url='/login/')
 def rooms(request):
     return render(request, 'chat/rooms.html')
 
 
+@login_required(login_url='/login/')
 def room(request, room_name):
     return render(request, 'chat/chatroom.html', {'room_name': room_name})
