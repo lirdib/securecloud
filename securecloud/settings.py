41080a98129cdadbf258d53be47bbde79af2e070
redhatalbania <38312870+lirdib@users.noreply.github.com>
2020-09-01 11:48:53 +0200

    chat app created

--- securecloud/settings.py
+++ securecloud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # django extensions
     'crispy_forms',
     'django_crispy_bulma',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     # own apps
     'authentication',
     'web',
+    'app'
 
 ]
 
